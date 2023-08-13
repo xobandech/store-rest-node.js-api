2bee57fbc3ef8ef97650383fe1d83fd3dee836fa
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-13 17:21:27 +0200

    Create store controllers

---
+++ controllers/storeController.js
@@ -0,0 +1,23 @@
+const getItems = (req, res) => {
+    
+}
+
+const getItemById = (req, res) => {
+    
+}
+
+const createPost = (req, res) => {
+    
+}
+
+const updateItem = (req, res) => {
+    
+}
+
+const deleteItem = (req, res) => {
+    
+}
+
+const searchForItems = (req, res) => {
+    
+}
