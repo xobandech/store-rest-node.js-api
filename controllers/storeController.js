0252466fa8ed4cadfa229a866f4b8eda134b69d1
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-14 16:41:02 +0200

    FIx createItem function

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,26 +2,25 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 const getItems = async (req, res) => {
-  await prisma.storeItem.findMany().then(response => res.send(response));
+  await prisma.storeItem.findMany().then((response) => res.send(response));
 };
 
 const getItemById = async (req, res) => {
   const { id } = req.params;
   await prisma.storeItem
     .findUnique({ where: { id: +id } })
-    .then(response => res.send(response))
+    .then((response) => res.send(response));
 };
 
 const createItem = async (req, res) => {
-  const { title, price, description, categoryId } = req.body;
-
+  const { title, description, price, categoryId } = req.body;
   try {
     const newItem = await prisma.storeItem.create({
       data: {
         title,
-        description,
         price,
-        category: { connect: { id: categoryId } },
+        description,
+        categoryId,
       },
     });
 
@@ -37,20 +36,50 @@ const createItem = async (req, res) => {
 const updateItem = async (req, res) => {
   const { id } = req.params;
   const { title, price, description, categoryId } = req.body;
-  await prisma.storeItem.update({
-    where: { id: id },
-    data: {
-      title: title && title,
-      price: price && price,
-      description: description && description,
-      categoryId: categoryId,
-    },
-  });
+  const updateData = {};
+
+  if (title) {
+    updateData.title = title;
+  }
+
+  if (price) {
+    updateData.price = price;
+  }
+
+  if (description) {
+    updateData.description = description;
+  }
+
+  if (categoryId) {
+    updateData.categoryId = categoryId;
+  }
+
+  try {
+    const updatedItem = await prisma.storeItem.update({
+      where: { id: +id },
+      data: updateData,
+    });
+
+    res.status(200).json(updatedItem);
+  } catch (error) {
+    console.error("Error updating item:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while updating the item." });
+  }
 };
 
 const deleteItem = async (req, res) => {
   const { id } = req.params;
-  await prisma.storeItem.delete({ where: { id } });
+  try {
+    const deletedItem = await prisma.storeItem.delete({ where: { id } });
+    res.status(200).json(deletedItem);
+  } catch (e) {
+    console.error("Error deleting items", e);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the item." });
+  }
 };
 
 const searchForItems = async (req, res) => {};
