a6321688266c26dc4906d63dd6865e0996a63c08
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-14 16:40:39 +0200

    Add bodyParser middleware

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const router = require('./routes/storeRoutes')
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(router)
 app.use(morgan('dev'))
+app.use("/", router)
 
 app.listen('3000', () => console.log("Server running on port 3000"))
