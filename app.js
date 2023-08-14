fce5b22040a940c54ec239e3130d517069f29605
Oleksandr Shtefan <alex.shtefan3333@gmail.com>
2023-08-14 14:36:47 +0200

    Add router middleware

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const router = require('./routes/storeRoutes')
 const app = express()
 
-app.use()
+app.use(router)
 app.use(morgan('dev'))
 
-app.listen('3001', () => console.log("Server running on port 3000"))
+app.listen('3000', () => console.log("Server running on port 3000"))
